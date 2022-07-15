e4037981a3302353d19cb31e9229b201d632c7f5
Felix Kyun <iamfelixkyun@gmail.com>
2022-07-16 00:14:26 +0530

    Daily Backup [12:14:26 AM | 16 July 2022]

---
+++ .screenlayout/monitor.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VGA1 --primary --mode 1024x768 --pos 0x0 --rotate normal --output HDMI1 --off --output DP1 --off
---
+++ .screenlayout/tv.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VGA1 --off --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off
